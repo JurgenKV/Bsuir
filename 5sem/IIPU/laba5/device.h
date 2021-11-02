4be2164b37d45cbe6045d34a0bffb6ea31a561c1
Yuri Kireev <70061113+JurgenKV@users.noreply.github.com>
2021-11-02 17:05:17 +0300

    Add files via upload

---
+++ 5sem/IIPU/laba5/device.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#pragma comment(lib, "setupapi.lib")
+#include <Windows.h>
+#include <Setupapi.h>
+#include <iostream>
+#include <Cfgmgr32.h>
+#include <vector>
+#include <string>
+#include <dbt.h>
+#include <initguid.h>
+#include <Usbiodef.h>
+
+class Device
+{
+	std::wstring HARDWARE_ID;
+	std::wstring name;
+	std::wstring devicePath;
+	std::wstring pid;
+	bool ejectable;
+	DEVINST devInst;
+
+	HDEVNOTIFY notificationHandle;
+	void register_handle(HWND hWnd);
+	void unregister() const;
+public:
+	static std::vector<Device> devices;
+
+	static void remove(const Device& device);
+
+	Device(PDEV_BROADCAST_DEVICEINTERFACE info, HWND hWnd = nullptr);
+
+	Device(HDEVINFO deviceList, SP_DEVINFO_DATA deviceInfo, HWND hWnd);
+
+	Device(const Device& other);
+
+	bool operator ==(const Device& other) const { return this->pid == other.pid; }
+
+	bool isEjectable() const { return this->ejectable; }
+
+	void eject() const;
+
+	void print() const;
+
+	std::wstring getName() const { return name; };
+};
